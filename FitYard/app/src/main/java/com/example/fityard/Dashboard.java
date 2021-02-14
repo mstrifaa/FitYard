93a56aefb08dbc0e954910c9cea390d1dd1d4df1
mstrifaa <46477436+mstrifaa@users.noreply.github.com>
2021-02-15 00:34:20 +0600

    dashboard implemented

--- FitYard/app/src/main/java/com/example/fityard/Dashboard.java
+++ FitYard/app/src/main/java/com/example/fityard/Dashboard.java
@@ -3,18 +3,31 @@ package com.example.fityard;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class Dashboard extends AppCompatActivity {
-    CardView pedoCard;
+    CardView BMICalculator, workout, nutritionGuide, dietPlans;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        pedoCard = (CardView) findViewById(R.id.pedoProgressCard);
+       BMICalculator = (CardView) findViewById(R.id.BMICalculator);
+       workout = (CardView) findViewById(R.id.workout);
+       nutritionGuide = (CardView) findViewById(R.id.nutritionGuide);
+       dietPlans = (CardView) findViewById(R.id.dietPlans);
 
+
+       BMICalculator.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent calculateBMI = new Intent(getApplicationContext(), calculateBMI.class);
+                startActivity(calculateBMI);
+           }
+       });
     }
 }
---
+++ FitYard/app/src/main/java/com/example/fityard/calculateBMI.java
@@ -0,0 +1,16 @@
+package com.example.fityard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class calculateBMI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculate_b_m_i);
+
+
+    }
+}
